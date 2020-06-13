91799ca5427f150723201eaf7d70042b7e070c4e
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-06-13 01:55:41 -0500

    Reinicio de contraseña email app

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use App\Category;
 use App\Product;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
