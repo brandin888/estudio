ef5952bd1bd241edb38fb550d49363867bc3532e
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-06-15 19:18:17 -0500

    contacto

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
+
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
         <script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js " > </script> 
         <script src="sweetalert2/dist/sweetalert2.all.min.js"></script>
@@ -38,7 +40,7 @@
         
 
 
-        
+         <script  src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js"> </script>    
 
         
         <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
