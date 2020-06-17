ef134fba47e48f46e7cf6c45ad97e4055b838240
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-06-16 19:20:06 -0500

    imagen_factura

--- resources/views/my-orders.blade.php
+++ resources/views/my-orders.blade.php
@@ -75,7 +75,7 @@
                         </div>
                         <div>
                             <div class="order-header-items">
-                                <div><img src="{{productImage( $order->imagen_factura) }}" alt="Product Image"></div>
+                                <div><img src="$order->imagen_factura)" alt="Product Image"></div>
                                 
                                 
                             </div>
