97078ff81055b65076ddc4c4b0f5311488e1ddb1
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-06-17 03:08:43 -0500

    facebook messenger

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -793,6 +793,31 @@ a {
          background-size: 100% 100%;
           z-index: -1;
         ">
+        <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v7.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="2174827419500879"
+  logged_in_greeting="Bienvenido a el mayorista. ¿En qué podemos ayudarte?"
+  logged_out_greeting="Bienvenido a el mayorista. ¿En qué podemos ayudarte?">
+      </div>
         <div id="app" >
             <header class="with-background" style="background-image: url('img/parallax/parallax9.jpg'); background-attachment: fixed;
   background-position: center;
