22a7742f5d05437b79c50f3a5640aba0c55774bc
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-06-17 02:04:57 -0500

    facebook

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -54,10 +54,28 @@
         <script  src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js"> </script>    
         
         @yield('extra-css')
+          <!-- Facebook Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '620489468811105');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=620489468811105&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
     </head>
 
 
 <body class="@yield('body-class', '')" >
+      
     @include('partials.nav')
 
     @yield('content')
