c7867bdbed45a7e4e478495fabf7ce1e4025d8e4
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-06-17 02:10:07 -0500

    facebook

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -770,6 +770,23 @@ a {
         <link rel="stylesheet" href="{{ asset('css/app.css?v=1.4') }}">
         <link rel="stylesheet" href="{{ asset('css/responsive.css?v=1.4') }}">
         <link rel="stylesheet" href="{{ asset('css/algolia.css') }}">
+        <!-- Facebook Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '620489468811105');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=620489468811105&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Facebook Pixel Code -->
     </head>
     <body style="background-image: url('img/parallax/image.jpg'); 
         background-repeat: no-repeat;
