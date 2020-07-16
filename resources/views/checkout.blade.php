e992864959534bf15e70f56d417821b4413aac98
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-07-15 19:44:47 -0500

    fixed

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -388,4 +388,7 @@
 
         })();
     </script>
+    <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+    <script src="{{ asset('js/algolia.js') }}"></script>
 @endsection
