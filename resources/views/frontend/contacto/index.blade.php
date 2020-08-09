e4e65daf763855474b4c6a1e10d7aa902327b191
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-08-09 14:53:28 -0500

    pricemayor

--- resources/views/frontend/contacto/index.blade.php
+++ resources/views/frontend/contacto/index.blade.php
@@ -207,6 +207,8 @@
 </script>
 <!-- <script type="text/javascript" src="{{ url('/') }}/js/contacto.js"></script> -->
 <script type="text/javascript" src="{{ asset('../resources/js/scripts/contacto.js')}}"></script>
-
+ <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
+  <script src="{{ asset('js/algolia.js?v=1.2') }}"></script>
 
 @stop
