3f41b9cc64c7cada7840e45ac9ca1fe1a30a4d9e
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-08-09 16:16:50 -0500

    algolia

--- resources/views/frontend/contacto/index.blade.php
+++ resources/views/frontend/contacto/index.blade.php
@@ -210,5 +210,5 @@
  <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
   <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
   <script src="{{ asset('js/algolia.js?v=1.2') }}"></script>
-
+<script src="js/app.js"></script>
 @stop
