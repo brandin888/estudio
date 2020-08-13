8d35e33eb1dfb994c87fc95350a6f44ed82f5127
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-08-12 19:35:59 -0500

    product

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -54,6 +54,11 @@
         <div class="product-section-information">
             <h1 class="mb-0 product-title">{{ $product->name }}</h1>
             <div style="margin-bottom:40px">{!! $stockLevel !!}</div>
+            @if($item)    
+                            
+                            <button type="submit" class="btn btn__enviar2" ><i class="fas fa-shopping-cart"></i> Ir al carrito</button>
+                           
+             @endif
             <div class="card-body d-lg-flex pb-5 shadow" style="background-color:#f7f7f9;font-size:18px;border-bottom:5px solid var(--primary)">
                 <div class="col-12 col-lg-6 mr-5 px-0">
                     @if($product->codigo_producto)
@@ -69,11 +74,7 @@
                     {{ $product->details }}
                 </div>
                 <div class="col-12 col-lg-6 dataproduct px-0">
-                            @if($item)    
-                            <div class="mb-3">
-                            <button type="submit" class="btn btn__enviar2" ><i class="fas fa-shopping-cart"></i> Ir al carrito</button>
-                            </div>
-                            @endif
+                            
                     <div class="mb-3">Precio : {{ $product->presentPriceUnidad() }}</div>
                     @if($product->pricemayor > 0)
                     <div class="product-section-subtitle" style="color:#171260">Precio por mayor: {{ $product->presentPriceMayor() }}</div>
