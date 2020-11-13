b429445ffc7b905621b85bc4c737a20efe3907f0
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-11-12 20:28:38 -0500

    contacto

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
-
+        <link href="{{ asset('css/algolia.css?v=1.0') }}" rel="stylesheet">
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
         <script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js " > </script> 
@@ -46,7 +46,11 @@
        
         
         @yield('extra-css')
-              
+        <style type="text/css">
+          header{
+            height: 0px;
+          }
+        </style>     
     </head>
 
 
