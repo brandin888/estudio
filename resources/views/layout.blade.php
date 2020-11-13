38727f57f080619b6e69a04910f486fc63531759
brandin888 <36492528+brandin888@users.noreply.github.com>
2020-11-12 20:18:25 -0500

    contacto

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <!-- Chola -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,19 +12,15 @@
         <meta name="theme-color" content="#353535" />
         <meta name="description" content="Somos una Distribuidora, hacemos ventas por mayor a todo Perú,  puedes encontrarnos en  Lima Cercado">
 
-       <link rel="shortcut icon" href="{{ asset('img/diseño/icono.png') }}" />
-      
+        <link rel="shortcut icon" href="{{ asset('img/diseño/icono.png') }}" />
+
         <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/additional-methods.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
 
-         
+        
         <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
         <script src = " https://unpkg.com/sweetalert/dist/sweetalert.min.js " > </script> 
         <script src="sweetalert2/dist/sweetalert2.all.min.js"></script>
@@ -39,35 +34,23 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css?v=1.6') }}">
         <link rel="stylesheet" href="{{ asset('css/responsive.css?v=1.4') }}">
-        <link href="{{ asset('css/algolia.css?v=1.0') }}" rel="stylesheet">
         <link href="{{ asset('css/all.css?v=1.1') }}" rel="stylesheet">
+        
 
         
         
-        <script  src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js"> </script>
-        <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-          
+
+
+        <script  src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js"> </script>   
+
+       
         
         @yield('extra-css')
-        <style type="text/css">
-        .page-item.active .page-link {
-          z-index: 3;
-          color: #fff;
-          background-color: #ff5a00;
-          border-color: #ff5a00;
-        }
-        .redes__link{
-          color: black;
-        }
-
-        header{
-          height: 0px;
-        }
-        </style>
+              
     </head>
 
- <div  style="background-color: rgba(255, 255, 255, 0.84);">
-<body class="@yield('body-class', '')" style="background-color: white">
+
+<body class="@yield('body-class', '')" >
     <!-- Load Whatsapp -->
     <div>
       <a class="appWhatsapp" target="_blanck" href="https://api.whatsapp.com/send?phone=51994084591&Hola&nbsp;MickySRL&nbsp;quisiera&nbsp;cotizar&nbsp;el&nbsp;siguiente&nbsp;producto">
@@ -76,16 +59,8 @@
     </div>
     <!-- end Whatsapp -->
 
- 
-   
-      <!-- Your Chat Plugin code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="111473320648629"
-  theme_color="#ff5a00"
-  logged_in_greeting="Bienvenido a MickySRL. ¿Cuál es el producto que desea cotizar?"
-  logged_out_greeting="Bienvenido a MickySRL  . ¿Cuál es el producto que desea cotizar?">
-      </div>
+    
+    
       
     @include('partials.nav')
 
@@ -93,17 +68,13 @@
 
     @include('partials.footer')
 
-</div>
-   <script src="https://cdn.jsdelivr.net/algoliasearch/3/algoliasearch.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/autocomplete.js/0/autocomplete.min.js"></script>
-  <script src="{{ asset('js/algolia.js?v=1.2') }}"></script>
-<script src="js/app.js"></script>
+
+   
     <script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
     <script type="text/javascript" src="//maps.google.com/maps/api/js?key=AIzaSyC4yN2BPmYWLa-GjR-5Z8V5YfkKgwygEF8"></script>
     <script src="{{ asset('js/gmaps.js')}}"></script>
 <script type="text/javascript">
-
   //Active Menu
 
 var  pestana = $('#pestana_vista').attr('valor');
@@ -126,6 +97,5 @@ function menu() {
     @yield('extra-js')
 
 </body>
-
-
 </html>
+</script>
