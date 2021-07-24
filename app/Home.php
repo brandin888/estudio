c5a2f42046ce2e61ab5d7e4b37307eb2b0f76220
brandin888 <36492528+brandin888@users.noreply.github.com>
2021-07-23 19:12:03 -0500

    hola

--- app/Home.php
+++ app/Home.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Home extends Model
 {
     protected $table = 'home';
+    protected $fillable = ['feature-title1', 'slug', 'description', 'featured_image'];
+protected $guarded = [];
 }
